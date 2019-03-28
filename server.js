476b3df1a6b29404b1a54b27a21cbcb3bbbe0eb9
Namgyu Ho <itsnamgyu@gmail.com>
2019-03-28 12:54:12 -0700

    Serve static files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express= require('express');
 const app = express();
 
+app.use('/static', express.static('static'));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
