4e3e0825cf6e30464ceb5fef2da7f83574164c91
Namgyu Ho <itsnamgyu@gmail.com>
2019-03-29 15:45:06 -0700

    Implement basic user console with CR-D

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,91 @@
 const app = {};
 
-app.UserModel = Backbone.Model.extend({
-    defaults: {
-        id: 0,
-        name: '',
-        email: '',
+app.User = Backbone.Model.extend({
+    defaults: function () {
+        return {
+            id: Math.floor(Math.random() * 1000000000),
+            name: '',
+            email: '',
+        };
+    },
+});
+
+
+app.UserCollection = Backbone.Collection.extend({
+    model: app.User,
+    url: '/api/user',
+});
+app.userCollection = new app.UserCollection();
+
+
+app.UserView = Backbone.View.extend({
+    template: _.template($('#user-template').html()),
+    initialize: function () {
+        this.model.on('change', this.render, this);
+        this.model.on('destroy', this.remove, this);
+    },
+    events: {
+        'click .user-delete': 'onDelete',
+    },
+    render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    },
+    onDelete: function () {
+        this.model.destroy();
     }
 });
 
-app.UserCollection = Backbone.Collection.extend({
-    model: app.UserModel,
-    url: 'api/user/',
+
+app.NewUserView = Backbone.View.extend({
+    el: $('#new-user'),
+    events: {
+        'submit form': 'onSubmit',
+    },
+    initialize: function () {
+        this.nameInput= this.$('#new-user-name');
+        this.emailInput = this.$('#new-user-email');
+    },
+    onSubmit: function (e) {
+        e.preventDefault();
+        app.userCollection.create(this.getAttributesFromInput());
+        this.refreshInputs();
+        console.log('Submit!');
+        return false;
+    },
+    getAttributesFromInput: function () {
+        return {
+            name: this.nameInput.val().trim(),
+            email: this.emailInput.val().trim(),
+        }
+    },
+    refreshInputs: function () {
+        this.nameInput.val('');
+        this.emailInput.val('');
+    },
 });
 
-app.UserView = Backbone.View.extend({
+
+app.UserListView = Backbone.View.extend({
+    el: $('#user-list'),
+    initialize: function() {
+        console.log('init user list!');
+        app.userCollection.on('add', this.addUser, this);
+        app.userCollection.on('reset', this.refresh, this);
+    },
+    addUser: function (userModel) {
+        console.log('Adding user!');
+        this.$el.append(new app.UserView({
+            model: userModel,
+        }).render().el);
+    },
+    refresh: function () {
+        app.userCollection.each(this.addUser, this);
+    }
 });
 
-collection = app.UserCollection();
-collection.fetch();
-collection.toString();
+app.newUserView = new app.NewUserView();
+app.userListView = new app.UserListView();
+app.userCollection.fetch();
+
 
