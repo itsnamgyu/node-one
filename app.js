d333e2e69827ae74a048ee37880d53e5a018fb33
Namgyu Ho <itsnamgyu@gmail.com>
2019-03-28 17:29:10 -0700

    Implement user api w/ basic CRUD ops
    + Tested by hand!

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express= require('express');
 const app = express();
 
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use('/static', express.static('public'));
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({'extended': false}));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use('/api/user', require('./routes/api'));
 
 app.listen(8081, function () {
     console.log('Starting server at port 8081...');
---
+++ routes/api.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const _ = require('underscore');
+
+const router = express.Router();
+const Users = require('../user');
+
+router.route('/')
+    .get((req, res) => {
+        Users.getAll()
+            .then((users) => {
+                res.json(_.values(users));
+            })
+            .catch((e) => {
+                console.log(e.stack);
+                res.sendStatus(500);
+            });
+    })
+    .post((req, res) => {
+        const user = req.body;
+        if (!Users.validateUser(user)) {
+            console.log('invalid!');
+            res.status(400).json({
+                'error_message': 'invalid user schema',
+            });
+            return;
+        }
+        Users.getAll()
+            .then((users) => {
+                users[user.id] = user;
+                return users;
+            })
+            .then((updatedUsers) => {
+                Users.saveAll(updatedUsers)
+                    .then(() => {
+                        res.json(user);
+                    });
+            })
+            .catch((e) => {
+                console.log(e.stack);
+                res.sendStatus(500);
+            })
+    });
+
+router.route('/:userId')
+    .get((req, res) => {
+        const id = req.params.userId;
+        Users.getAll()
+            .then((users) => {
+                if (id in users) {
+                    res.json(users[id]);
+                } else {
+                    res.status(400).json({
+                        'error_message': 'could not find user',
+                    });
+                }
+            })
+    })
+    .post((req, res) => {
+        const user = req.body;
+        if (!Users.validateUser(user)) {
+            res.status(400).json({
+                'error_message': 'invalid user schema',
+            });
+            return;
+        }
+        Users.getAll()
+            .then((users) => {
+                users[user.id] = user;
+                return users;
+            })
+            .then((updatedUsers) => {
+                Users.saveAll(updatedUsers)
+                    .then(() => {
+                        res.json(user);
+                    });
+            })
+            .catch((e) => {
+                console.log(e.stack);
+                res.sendStatus(500);
+            });
+    })
+    .delete((req, res) => {
+        const id = req.params.userId;
+        Users.getAll()
+            .then((users) => {
+                if (id in users) {
+                    const userString = JSON.stringify(users[id]);
+                    delete users[id];
+                    Users.saveAll(users)
+                        .then(() => {
+                            res.send(userString);
+                        });
+                } else {
+                    res.status(400).json({
+                        'error_message': 'could not find user',
+                    });
+                }
+            })
+            .catch((e) => {
+                console.log(e.stack);
+                res.status(500);
+            })
+    });
+
+module.exports = router;
